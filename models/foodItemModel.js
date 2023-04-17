12e51f90e30e016680314189c4738d992ef87f72
Leo Gavin <leogavin123@outlook.com>
2023-04-17 13:32:26 +0530

    food item controller updated

--- models/foodItemModel.js
+++ models/foodItemModel.js
@@ -1,16 +1,29 @@
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const FoodItemSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  quantity: { type: Number, required: true },
+  name: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
   mealType: {
     type: String,
     enum: ["breakfast", "lunch", "dinner"],
     required: true,
   },
   canteen: {
-    type: mongoose.Schema.Types.ObjectId,
+    type: Schema.Types.ObjectId,
     ref: "Canteen",
+  },
+  imageUrl: {
+    type: String,
+  },
+  price: {
+    type: Number,
     required: true,
   },
 });
